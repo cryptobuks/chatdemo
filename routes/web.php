2bcd0d35d8fe044de52d30d93a09a32902f4849d
Crocho de ICT <crochodeict@gmail.com>
2017-02-09 12:59:09 +0700

    Story 1: Part 3 from YouTube

---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/chat', function() {
+    return view('chat');
+})->middleware('auth');
+
+Route::get('/messages', function() {
+    return App\Message::with('user')->get();
+})->middleware('auth');
+
+Route::post('/messages', function() {
+    // Store the new message
+    $user = Auth::user();
+    $user->messages()->create([
+        'message' => request()->get('message')
+    ]);
+
+    return ['status' => 'OK'];
+    // return App\Message::with('user')->get();
+})->middleware('auth');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
